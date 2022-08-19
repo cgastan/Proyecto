f13a0c04ff18cb9b4eb032a0443ed00e57206a5c
Camila Gastán <110438229+cgastan@users.noreply.github.com>
2022-08-19 09:34:40 -0300

    products
    autos

---
+++ js/products.js
@@ -0,0 +1,45 @@
+let currentProductsArray = [];
+const AUTOS_URL="https://japceibal.github.io/emercado-api/cats_products/101.json"
+function showProductsList(array) {
+    let htmlContentToAppend = "";
+    for(let product of array.products){
+        htmlContentToAppend += `
+      
+    <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="${product.image}" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>${product.name}-${product.currency} ${product.cost}</h4> 
+                        <p> ${product.description}</p> 
+                        </div>
+                        <small class="text-muted">${product.soldCount} vendidos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    
+        `
+        document.getElementById("productos").innerHTML = htmlContentToAppend;
+
+    }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(AUTOS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            currentProductsArray = resultObj.data;
+            showProductsList(currentProductsArray);
+        }
+    });
+});
+
+
+
+
