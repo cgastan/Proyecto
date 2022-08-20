0befbb9d905bfa2f812685b875ee9ba202f98ea0
Camila Gastán <110438229+cgastan@users.noreply.github.com>
2022-08-20 18:30:20 -0300

    Login

---
+++ js/login.js
@@ -0,0 +1,28 @@
+function alertaEmail() {
+    document.getElementById("email0").innerHTML = `Ingresa tu e-mail`;
+    document.getElementById("email").style.borderColor = "red";
+}
+function alertaContrasena() {
+    document.getElementById("pass0").innerHTML = `Ingresa tu e-mail`;
+    document.getElementById("contrasena").style.borderColor = "red";
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("ingresar").addEventListener("click", function () {
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("contrasena").value;
+        let datosCompletos = true;
+        if (email == "") {
+            datosCompletos = false;
+            alertaEmail()
+        }
+        if (password == "") {
+            datosCompletos = false;
+            alertaContrasena();
+        }
+        if (datosCompletos) {
+            window.location = "portada.html";
+        }
+    })
+})
