6d527bd1dd026e0700d846f7879c8deeb666b8e2
Camila Gastán <110438229+cgastan@users.noreply.github.com>
2022-09-18 22:20:51 -0300

    ComentariosSinDesafio

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,9 @@
 const CURRENT_PRODUCT_INFO_URL = PRODUCT_INFO_URL.valueOf() + localStorage.getItem("prodID") + ".json";
-const CURRENT_COMMENTS_URL= PRODUCT_INFO_COMMENTS_URL.valueOf()+ localStorage.getItem("prodID") + ".json";
-let htmlContentToAppend=``
+const CURRENT_COMMENTS_URL = PRODUCT_INFO_COMMENTS_URL.valueOf() + localStorage.getItem("prodID") + ".json";
+let productInfo= ``
+let commentsInfo=``
+let htmlContentToAppend = ``
+let htmlComments = ``
 
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(CURRENT_PRODUCT_INFO_URL).then(function (resultObj) {
@@ -8,9 +11,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
             productInfo = resultObj.data;
             showProductInfo(productInfo);
         }
-    }).then(getJSONData(CURRENT_COMMENTS_URL).then(function(resultCommentsObj){
-        if (resultCommentsObj.status==="ok"){
-            commentsInfo=resultCommentsObj.data;
+    }).then(getJSONData(CURRENT_COMMENTS_URL).then(function (resultCommentsObj) {
+        if (resultCommentsObj.status === "ok") {
+            commentsInfo = resultCommentsObj.data;
             showComments(commentsInfo);
         }
     }));
@@ -43,39 +46,77 @@ document.addEventListener("DOMContentLoaded", function (e) {
             </div>
             </div>`
         }
-        htmlContentToAppend+=`</div>`
+        htmlContentToAppend += `</div>`
         document.getElementById("divProductInfo").innerHTML = htmlContentToAppend;
     }
 });
 
-function showComments(array){
+function showComments(array) {
+    htmlContentToAppend += `
+    <br>
+    <h3 class="p4">Comentarios</h3>
+    <div class="card" >
+    <ul class="list-group">`
+    for (let comment of array) {
+        htmlContentToAppend +=
+            `<li class="list-group-item">
+    <strong>${comment.user}</strong><p class="text-muted">${comment.dateTime}-`; showStarsRate(comment.score); htmlContentToAppend += `</p>
+    <p class="text-muted">${comment.description}</p>
+     </li>`
+        htmlContentToAppend +=
+            `</ul>
+    </div>`
+    }
     htmlContentToAppend+=`
     <br>
-    <h3 class="p4">Comentarios</h2>
-    <div class="card" style="width: 18rem;">
-    <ul class="list-group list-group-flush">`
-    for(let comment of array){
-        htmlContentToAppend+=
-        `<li class="list-group-item">
-        <strong>${comment.user}</strong><p class="text-muted">-${comment.dateTime}-${showStarsRate(comment.score)}</p>
-        <p class="text-muted">${comment.description}</p>
-        </li>`
-    htmlContentToAppend+=
-    `</ul>
-</div>`
-}
+    <h3 class="p4">Comentar</h3>
+    <div class="mb-3">
+    <label for="userComment" class="form-label">Tu opinión</label>
+    <textarea name="userComment" class="form-control w-50" id="userComment"rows="5"></textarea>
+    </div>
+    <div class="mb-3">
+    <label for="userScore" class="form-label">Tu puntuación</label>
+    <select class="form-select" id="userScore">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    </select>
+    </div>
+    <button class="btn btn-primary" type="submit" id="enviarComentario">Enviar</button>
+    `
     document.getElementById("divProductInfo").innerHTML = htmlContentToAppend;
+    /* Parte del desafio para despues
+     document.getElementById("enviarComentario").addEventListener("click",function(){
+        addComment();
+        showComments(commentsInfo); 
+    })*/
 }
 
-function showStarsRate(number){
-let htmlComments=``
-   let i=0;
-   let x=5-i
-   while (i<=5 && i<=number){
-    i++;
-    htmlComments+=`<span class="fa fa-star checked"></span>`
-   }if(x>0){
-   htmlComments+=`<span class="fa fa-star"></span>`
-   }
-   htmlContentToAppend+=htmlComments;
+function showStarsRate(scoreNumber) {
+    let htmlComments = ``;
+    let x = 5 - scoreNumber;
+    if (scoreNumber <= 5) {
+        htmlComments += (`<span class="fa fa-star checked"></span>`).repeat(scoreNumber);
+    } if (scoreNumber < 5 && x > 0) {
+        htmlComments += (`<span class="fa fa-star"></span>`).repeat(x);
+    }
+
+    htmlContentToAppend += htmlComments;
 }
+
+
+
+
+/* Parte del desafio para despues
+function addComment(){
+    let comentario=document.getElementById("userComment").valueOf;
+    let puntuacion=parseInt(document.getElementById("userScore").valueOf());
+    commentsInfo.push(
+    {product: localStorage.getItem("prodID"),
+    score: puntuacion,
+    description: comentario,
+    user: localStorage.getItem("usuarioMail"),
+    dateTime: "2022-05-21 23:10:41"});
+} */
